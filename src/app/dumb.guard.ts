810beefca429461483f8bd9ee25e59f6549be6bb
primo-malvado <rui.fajozes@gmail.com>
2024-01-24 12:21:32 +0000

    line

--- src/app/dumb.guard.ts
+++ src/app/dumb.guard.ts
@@ -1,6 +1,12 @@
 import { CanActivateFn } from '@angular/router';
 
 export const dumbGuard: CanActivateFn = (route, state) => {
-  alert("you cannot access to this route")
-  return false;
+  let canEnter = false;
+
+  if(canEnter){
+    return true; 
+  }else{
+    alert("you cannot access to this route")
+    return false;
+  }
 };
